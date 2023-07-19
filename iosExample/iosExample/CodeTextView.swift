e7c73559f8078d9394a1585b9306ec6ef0727c8d
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2023-07-19 08:33:59 +0200

    Completed ios example app

--- iosExample/iosExample/CodeTextView.swift
+++ iosExample/iosExample/CodeTextView.swift
@@ -6,29 +6,32 @@
 //
 
 import SwiftUI
-//import shared
+import shared
 
-//struct CodeTextView: UIViewControllerRepresentable {
-//    @Binding private var highlights: Highlights
-//
-//    init(newHighlights: Binding<Highlights>) {
-//        self._highlights = newHighlights
-//    }
-//
-//    func makeUIViewController(context: Context) -> UIViewController {
-//        return UIViewController()
-//    }
-//
-//    func updateUIViewController(_ wrapper: UIViewController, context: Context) {
-//        let kotlinController = HighlightsViewKt.CodeTextViewUiViewController(highlights: highlights)
-//        // Cleanup
-//        kotlinController.removeFromParent()
-//        kotlinController.view.removeFromSuperview()
-//        // Update view
-//        wrapper.addChild(kotlinController)
-//        wrapper.view.addSubview(kotlinController.view)
-//        // Match with frame
-//        kotlinController.view.frame = wrapper.view.frame
-//        kotlinController.didMove(toParent: wrapper)
-//    }
-//}
+// Omit KMM library prefixing
+typealias Highlights = HighlightsHighlights
+
+struct CodeTextView: UIViewControllerRepresentable {
+    @Binding private var highlights: Highlights
+
+    init(newHighlights: Binding<Highlights>) {
+        self._highlights = newHighlights
+    }
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        return UIViewController()
+    }
+
+    func updateUIViewController(_ wrapper: UIViewController, context: Context) {
+        let kotlinController = HighlightsViewKt.CodeTextViewUiViewController(highlights: highlights)
+        // Cleanup
+        kotlinController.removeFromParent()
+        kotlinController.view.removeFromSuperview()
+        // Update view
+        wrapper.addChild(kotlinController)
+        wrapper.view.addSubview(kotlinController.view)
+        // Match with frame
+        kotlinController.view.frame = wrapper.view.frame
+        kotlinController.didMove(toParent: wrapper)
+    }
+}
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,6 +26,17 @@ kotlin {
         }
     }
 
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
     sourceSets {
         // Common
         val commonMain by getting {
@@ -36,7 +47,7 @@ kotlin {
                 implementation(compose.ui)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
-                api("dev.snipme:highlights:0.2.0-SNAPSHOT")
+                api("dev.snipme:highlights:0.3.0-SNAPSHOT")
             }
         }
         val commonTest by getting {
@@ -47,95 +58,20 @@ kotlin {
         // Android
         val androidMain by getting {
             dependencies {
-                api("androidx.activity:activity-compose:1.7.2")
-                api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.10.1")
+                implementation("androidx.activity:activity-compose:1.7.2")
+                implementation("androidx.appcompat:appcompat:1.6.1")
+                implementation("androidx.core:core-ktx:1.10.1")
             }
+        }
+        // iOS
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
     }
 }
-
-//kotlin {
-//    android {
-//        compilations.all {
-//            kotlinOptions {
-//                jvmTarget = libs.versions.jvmTarget.get()
-//            }
-//        }
-//    }
-//
-//    ios {
-//        binaries {
-//            framework {
-//                baseName = "shared"
-//                isStatic = true
-//            }
-//        }
-//    }
-//
-////    listOf(
-////        iosX64(),
-////        iosArm64(),
-////        iosSimulatorArm64()
-////    ).forEach {
-////        it.binaries.framework {
-////            baseName = "shared"
-////            isStatic = true
-////        }
-////    }
-//
-//    sourceSets {
-//        val commonMain by getting {
-//            dependencies {
-//                implementation(compose.runtime)
-//                implementation(compose.foundation)
-//                implementation(compose.material)
-//                implementation(compose.ui)
-//                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-//                implementation(compose.components.resources)
-//            }
-//        }
-//        val commonTest by getting {
-//            dependencies {
-//                implementation(kotlin("test"))
-//            }
-//        }
-//        val androidMain by getting {
-//            dependsOn(commonMain)
-//            dependencies {
-//                implementation("dev.snipme:highlights-jvm:0.2.0-SNAPSHOT")
-//                api("androidx.activity:activity-compose:1.7.0")
-//                api("androidx.appcompat:appcompat:1.6.1")
-//                api("androidx.core:core-ktx:1.10.0")
-//            }
-//        }
-////        val androidUnitTest by getting
-////        val iosX64Main by getting
-////        val iosArm64Main by getting
-////        val iosSimulatorArm64Main by getting
-////        val iosMain by creating {
-////            dependsOn(commonMain)
-////            iosX64Main.dependsOn(this)
-////            iosArm64Main.dependsOn(this)
-////            iosSimulatorArm64Main.dependsOn(this)
-////        }
-////        val iosX64Test by getting
-////        val iosArm64Test by getting
-////        val iosSimulatorArm64Test by getting
-////        val iosTest by creating {
-////            dependsOn(commonTest)
-////            iosX64Test.dependsOn(this)
-////            iosArm64Test.dependsOn(this)
-////            iosSimulatorArm64Test.dependsOn(this)
-////        }
-//    }
-//}
-//
-//android {
-//    namespace = "pl.tkadziolka.highlights"
-//    compileSdk = 33
-//
-//    defaultConfig {
-//        minSdk = 24
-//    }
-//}
