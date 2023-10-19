5dc901bbcd1d0f02e4ab004ecb4228dbcd736322
tkadziolka <tmax0135@gmail.com>
2023-10-19 21:14:43 +0200

    Added Material3 field variant

--- kodeview/build.gradle.kts
+++ kodeview/build.gradle.kts
@@ -53,6 +53,7 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material)
+                implementation(compose.material3)
                 implementation(compose.ui)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
